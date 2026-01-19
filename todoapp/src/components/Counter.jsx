68ec7412975a5b18c9ddc1840192b9b5b9a31bd5
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-19 19:55:20 +0500

    Added Even or Odd condition through Ternary

--- todoapp/src/components/Counter.jsx
+++ todoapp/src/components/Counter.jsx
@@ -4,12 +4,12 @@ const Counter = () => {
   // const val = useState(0);
   // console.log(val)  This value returns an array that has two items, first the initial state of the variable and the second is the set function that updated the value.
   const [count, setCount] = useState(0);
-  const [value, setValue] = useState("JavaScript");
+  
   return (
     <>
       <p>Count : {count}</p>
-      <p>Value is : {value}</p>
-      <button onClick={() => setValue("React")}>Change</button>
+      <p>Number is : {count % 2 === 0 ? "Even": "Odd"}</p>
+     
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
   );
