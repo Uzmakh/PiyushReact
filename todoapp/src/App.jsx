ed3244f25d283ee6138f4f41cfa9851ba1bdcb21
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-19 20:27:33 +0500

    component has local state

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   return (
     <div className="todo-container">
       <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
       <Header title="Tasks" />
       <TodoItem text="Eat" />
       <TodoItem text="Go to bazar" completed={true} />
