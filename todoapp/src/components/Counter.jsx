44ca8f416b856262c52906241f4a70a477bcf147
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-20 22:16:55 +0500

    Introducing useState

--- todoapp/src/components/Counter.jsx
+++ todoapp/src/components/Counter.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 const Counter = () => {
+  // introducing useState
   // const val = useState(0);
   // console.log(val)  This value returns an array that has two items, first the initial state of the variable and the second is the set function that updated the value.
   const [count, setCount] = useState(0);
