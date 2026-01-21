8f2abf2b6af72853b1c2ba8d684dc0d8f39a06a5
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-21 22:17:48 +0500

    used map method to render posts data

--- practicetopics/src/App.jsx
+++ practicetopics/src/App.jsx
@@ -1,12 +1,26 @@
-import { Board } from "./TicTacToeGame/Board"
-import './App.css'
+import { useEffect, useState } from "react";
+import { getPosts } from "./CallingAPI/API";
+import { Board } from "./TicTacToeGame/Board";
+import { PostCard } from "./CallingAPI/API/components/PostCard";
+import "./App.css";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    getPosts().then((posts) => setData(posts));
+  }, []);
   return (
     <>
-      <h1>Tic Tac Toe Game</h1>
-      <Board />
+      {/* <h1>Tic Tac Toe Game</h1>
+      <Board /> */}
+      <div>
+        {data ? (
+          data.map((e) => <PostCard title={e.title} body={e.body} />)
+        ) : (
+          <p>No data</p>
+        )}
+      </div>
     </>
-  )
+  );
 };
 export default App;
