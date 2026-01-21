afcefcea05d9ef76106c525a7ad79e55e65fe1d7
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-21 20:07:13 +0500

    initial commit

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,12 @@
+import { Board } from "./TicTacToeGame/Board"
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <h1>Tic Tac Toe Game</h1>
+      <Board />
+    </>
+  )
+};
+export default App;
---
+++ tic-tac-toe/src/TicTacToeGame/Square.jsx
@@ -0,0 +1,7 @@
+export const Square = (props) => {
+  return (
+    <div className="square" onClick={props.onClick}>
+      <h5>{props.value}</h5>
+    </div>
+  );
+};
